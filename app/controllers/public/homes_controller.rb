d34e4070d7f8a1324d88633ee91e86ccfa46e419
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-02-19 22:07:56 +0900

    [TOPカスタム] 2-3

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,10 +3,13 @@ class Public::HomesController < ApplicationController
   def top
     @items = Item.all
     if params[:genre_id] === nil
-      @order_details =   OrderDetail.group(:item_id).order('count(item_id) desc').limit(3)
+      @order_details = OrderDetail.group(:item_id).order('count(item_id) desc').limit(5)
     else
       # byebug
-      @order_details = OrderDetail.where.Item.where(params[:genre_id])
+      @genre_items = Item.where(genre_id: params[:genre_id])
+      # byebug
+      @order_details = OrderDetail.group(item_id: @genre_items.ids.to_s)
+      # byebug
     end
     @genres = Genre.all
   end
